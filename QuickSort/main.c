13ed4917cb55f8b8f2cf8df63043eb55cb3de94f
Thiago Silva <thhzmonster@gmail.com>
2024-03-03 19:54:30 -0300

    implementando QuickSort

--- QuickSort/main.c
+++ QuickSort/main.c
@@ -78,36 +78,65 @@ void listaOrdenadaDecrescente(int *vetBubble, int tamVet)
 }
 
 // FUNÇÃO PRINCIPAL: BUBBLE SORT //
-void bubbleSort(int *vetBubble, int tamVet, long long *totalSwap, long long *totalInter, clock_t *tempoInicio, long long *totalIteracoes)
-{
-  
+void troca(int *a, int *b, long long *totalSwap) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+    (*totalSwap)++;
+}
+
+// FUNÇÃO DE PARTIÇÃO DO QUICKSORT //
+int particiona(int *vetBubble, int inicio, int fim, long long *totalSwap, long long *totalComp) {
+    int pivo = vetBubble[fim];
+    int i = (inicio - 1);
+
+    for (int j = inicio; j <= fim - 1; j++) {
+        (*totalComp)++;
+        if (vetBubble[j] < pivo) {
+            i++;
+            troca(&vetBubble[i], &vetBubble[j], totalSwap);
+        }
+    }
+    troca(&vetBubble[i + 1], &vetBubble[fim], totalSwap);
+    return (i + 1);
+}
+
+// FUNÇÃO QUICKSORT //
+void quickSort(int *vetBubble, int inicio, int fim, long long *totalSwap, long long *totalComp, clock_t *tempoInicio, long long *totalIteracoes) {
+    if (inicio < fim) {
+        int pivo = particiona(vetBubble, inicio, fim, totalSwap, totalComp);
+        quickSort(vetBubble, inicio, pivo - 1, totalSwap, totalComp, tempoInicio, totalIteracoes);
+        quickSort(vetBubble, pivo + 1, fim, totalSwap, totalComp, tempoInicio, totalIteracoes);
+    }
 }
 
 // MAIN //
-int main()
-{
+int main() {
     srand(10);
     clock_t tempoInicio;
     int vet[TAM_MAX1kk];
-    long long totalSwap, totalComp, totalnter;
+    long long totalSwap = 0, totalComp = 0, totalIteracoes = 0;
     int tamVet = TAM_MAX1kk;
-    //listaOrdenadaDecrescente(vet, tamVet);
+
+    // Escolha uma das opções de lista para testar o algoritmo
+    // listaOrdenadaDecrescente(vet, tamVet);
     // listaOrdenada(vet, tamVet);
-    //listaParcialmenteOrdenada75(vet, tamVet);
-    //createRandomList(vet, tamVet);
-    listaParcialmenteOrdenada50(vet, tamVet);
-    bubbleSort(vet, tamVet, &totalSwap, &totalComp, &tempoInicio, &totalnter);
-    for (int i = 0; i < tamVet; i++)
-    {
+    // listaParcialmenteOrdenada75(vet, tamVet);
+    // createRandomList(vet, tamVet);
+    // listaParcialmenteOrdenada50(vet, tamVet);
+
+    tempoInicio = clock();
+    quickSort(vet, 0, tamVet - 1, &totalSwap, &totalComp, &tempoInicio, &totalIteracoes);
+    tempoInicio = clock() - tempoInicio;
+
+    for (int i = 0; i < tamVet; i++) {
         printf("%d\n", vet[i]);
     }
 
     printf("Tempo em milisegundos: %f\n", (1000.0 * tempoInicio) / CLOCKS_PER_SEC);
     printf("Numero de trocas: %lld\n", totalSwap);
     printf("Numero de comparações: %lld\n", totalComp);
-    printf("Numero de interacoes: %lld\n", totalnter);
-
-    // bubbleSort(vet);
+    printf("Numero de interacoes: %lld\n", totalIteracoes);
 
     return 0;
 }
